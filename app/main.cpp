8fcd57c15a2f7ddc6178efc6916b4a3fff170df5
DMEN073 <kaiping7895@gmail.com>
2024-11-12 11:57:06 +0800

    minimum file structure with cmake for cpp

---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mylib.h"
+
+int main() {
+    std::cout << "Welcome to texteditor!" << std::endl;
+    mylib_function();
+    return 0;
+}
---
+++ src/mylib.cpp
@@ -0,0 +1,6 @@
+#include "mylib.h"
+#include <iostream>
+
+void mylib_function() {
+    std::cout << "Hello from MyLib!" << std::endl;
+}
